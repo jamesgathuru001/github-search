f1f222a56be4a2ea9f022671b56e270736c32b72
James <jamesgathuru001@gmail.com>
2019-05-05 23:50:29 +0300

    debugging code

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,2 +1,29 @@
-export class Github.Service {
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { User } from './user';
+import { Repos } from './repos';
+import { environment } from '../environments/environment';
+
+
+
+@Injectable()
+export class GithubService {
+    username: string;
+
+    constructor(private _http: HttpClient) {
+        console.log('Github Service Ready...');
+        this.username = 'jamesgathuru001';
+
+    }
+    getUser() {
+        return this._http.get('https://api.github.com/users/' + this.username);
+
+    }
+    getRepos() {
+        return this._http.get(' https://api.github.com/users/' + this.username + '/repos');
+    }
+    updateUser(username: string) {
+        this.username = username;
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: '459cd4446669f244f3ece09f5df31642ef81d008'
 };
 
 /*
